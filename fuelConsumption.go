4f47132341003838ce84fe0c959ed475e321c69b
Doniyor Ganiev <donik.ganiev05@mail.ru>
2020-01-09 06:30:05 +0300

    Fixed #5 - "я не совсем понял ваш issue, но сделал ,что смог"

--- fuelConsumption.go
+++ fuelConsumption.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	const distance = 100
+	const distOfConsFuel = 100
 	fuelConsume := 6
 	fuelAmount := 3
 
-	result := approxDistance(distance, fuelConsume, fuelAmount)
+	resDistance := approxDistance(distOfConsFuel, fuelConsume, fuelAmount)
 
-	fmt.Println(result, "km")
+	fmt.Println(resDistance, "km")
 }
 
-func approxDistance(distance int, fuelConsume int, fuelAmount int) int {
+func approxDistance(distOfConsFuel int, fuelConsume int, fuelAmount int) int {
 	var approxDistance int
 
-	approxDistance = distance * fuelAmount / fuelConsume
+	approxDistance = distOfConsFuel * fuelAmount / fuelConsume
 	tenPercent := approxDistance / 10
 
 	return approxDistance - tenPercent
--- fuelConsumption_test.go
+++ fuelConsumption_test.go
@@ -5,17 +5,17 @@ import "testing"
 func Test_approximateDistance(t *testing.T) {
 
 	tests := []struct {
-		name            string
-		distance        int
-		fuelConsumption int
-		fuelAmount      int
-		want            int
+		name           string
+		distOfConsFuel int
+		fuelConsume    int
+		fuelAmount     int
+		want           int
 	}{
 
 		{"Approximate Distance", 100, 6, 3, 45},
 	}
 	for _, test := range tests {
-		got := approxDistance(test.distance, test.fuelConsumption, test.fuelAmount)
+		got := approxDistance(test.distOfConsFuel, test.fuelConsume, test.fuelAmount)
 		if got != test.want {
 			t.Error("Fuel test", test.name, "got:", got, "want:", test.want)
 		}
